cd91b18af0177c77745ca691b8fd86b00dca5ff5
Trigram <ty500gt@gmail.com>
2024-05-03 12:26:16 +0200

    imported the JUnit library for unit test environment and created first test case

---
+++ test/TestChessEngine/TestChessBoard.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestChessEngine;
+
+import chessengine.Board;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+/**
+ *
+ * @author User
+ */
+public class TestChessBoard {
+    public void initialBoard()
+    {
+        final Board board = Board.createStandardBoard();
+        assertEquals(board.currentPlayer().getLegalMoves().size(),20);
+    }
+    
+}
